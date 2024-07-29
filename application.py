e3ec944e6782a4318f019f7737d3a0653630203c
duke141 <vtminhduc140101@gmail.com>
2024-07-29 15:41:10 +0700

    Adding register layout

--- application.py
+++ application.py
@@ -120,7 +120,42 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        
+        # Ensure comfirmation password was submitted
+        elif not request.form.get("confirmation"):
+            return apology("must provide confirmation password", 403)
+        
+        # Ensure password and confirmation is same
+        elif not request.form.get("password") == request.form.get("confirmation"):
+            return apology("the confirmation is not match", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) == 1:
+            return apology("username already exists", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+    
 
 
 @app.route("/sell", methods=["GET", "POST"])
